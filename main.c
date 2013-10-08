f07f0e2cc53caea7b172f1d24180a679554393b7
ricardogtx <ricardocdzg@hotmail.com>
2013-10-07 22:57:49 -0300

    funcao imprimirGride,tenstando

--- main.c
+++ main.c
@@ -2,6 +2,36 @@
 #include<stdlib.h>
 #include "GeoLib.c"
 
-#define gride 20
+#define tamanhoGride 20//tamanho teste
 
-main(){}
+void imprimeGride (){//lembrando que esta função somente irá imprimir!
+     int i,j;
+     char gride[tamanhoGride][tamanhoGride];//gride teste
+     
+          for(i=0;i<tamanhoGride;i++){
+          
+            for(j=0;j<tamanhoGride;j++){
+            if(i==0 && j==0){printf("0  ");}                            
+            else if(i==0){
+              if(j>9){printf("%d ",j);}       
+              else{printf(" %d ",j);}
+            }
+            else if(j==0){
+             if(i>9){printf("%d",i);}       
+              else{printf("%d ",i);}
+            }
+            else{
+            gride[i][j] =' ';//atribuição teste
+            printf("|%c ",gride[i][j]);
+            }
+            }
+          printf("\n");                     
+          }
+
+}
+
+main(){
+imprimeGride();
+
+system("pause");
+}
